47e97bfb89414e4c74cd0c7ef96b8c39dbac16d3
Emma0032 <emmanuelowoh032@gmail.com>
2023-06-12 00:33:36 +0100

    Added numeric otp verification

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use App\Models\User;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class VerificationController extends Controller
 {
@@ -38,5 +42,33 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    public function sendOtp(EmailVerificationRequest $request)
+    {
+        $user = User::find($request->user()->id);
+
+        $otp = Str::random(6); // Generate a random 6-digit OTP
+
+        // Save the OTP to the user's record in the database
+        $user->otp_code = $otp;
+        $user->save();
+
+        // Send the OTP to the user's email
+        Mail::to($user->email)->send(new OtpMail($otp));
+
+        return back()->with('otp_sent', true);
+    }
+
+    public function resend(Request $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return response(['message' => 'Email already verified.'], 400);
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        // Call the sendOtp method instead of the default behavior
+        return $this->sendOtp($request);
     }
 }
